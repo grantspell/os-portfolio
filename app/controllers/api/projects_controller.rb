44c54ac38a55b782c5187df64b9742fb64b9bf16
grantspell <spellgrant@gmail.com>
2018-01-07 15:09:49 -0500

    INSTALLED | NPM Packages : axios, styled-components, react-router-dom in `client`

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,2 +1,17 @@
 class Api::ProjectsController < ApplicationController
+
+    def index
+        @projects = Project.all
+
+        render json: @projects
+    end
+
+    def show
+        project_id = params[:id]
+
+        @project = Project.find_by_id(project_id)
+
+        render json: @project
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/projects", to: "projects#index", as: "projects"
+    get "/projects/:id", to: "projects#show", as: "project"
   end
   
 end
