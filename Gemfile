0adc4ff67cc3d0b6046c24223f53c94f935e06df
grantspell <spellgrant@gmail.com>
2018-02-28 14:53:20 -0500

    UDPATED | `ruby` version [again] in GemFile with proper version && UPDATED | `Dilettante` deployed + GitHub URLs in DB

--- Gemfile
+++ Gemfile
@@ -41,4 +41,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Ruby
-ruby '2.3.4'
+ruby '2.4.2'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-Project.create({ name: "Dilettante", liveURL: "http://project-four.herokuapps.com", githubURL: "n/a", previewURL: "n/a", iconURL: "http://icongal.com/gallery/image/9172/document_text_file_txt.png" })
+Project.create({ name: "Dilettante", liveURL: "https://project-four.herokuapp.com/", githubURL: "https://github.com/grantspell/project-four", previewURL: "n/a", iconURL: "http://icongal.com/gallery/image/9172/document_text_file_txt.png" })
