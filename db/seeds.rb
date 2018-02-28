41ad27fd2a065d4b8119a6bd35b60c422a4a9066
grantspell <spellgrant@gmail.com>
2018-02-28 15:04:28 -0500

    ADDED | `Hangman` project to `seeds.rb` in DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
-Project.create({ name: "Dilettante", liveURL: "https://project-four.herokuapp.com/", githubURL: "https://github.com/grantspell/project-four", previewURL: "n/a", iconURL: "http://icongal.com/gallery/image/9172/document_text_file_txt.png" })
+# Projects
+
+# Dilettante
+Project.create({
+    name: "Dilettante",
+    liveURL: "https://project-four.herokuapp.com/",
+    githubURL: "https://github.com/grantspell/project-four",
+    previewURL: "n/a",
+    iconURL: "http://icongal.com/gallery/image/9172/document_text_file_txt.png"
+})
+
+# Hangman
+Project.create({
+    name: "Hangman",
+    liveURL: "https://styx-hangman.bitballoon.com/",
+    githubURL: "https://github.com/grantspell/project_one",
+    previewURL: "n/a",
+    iconURL: "http://icongal.com/gallery/image/9172/document_text_file_txt.png"
+})
