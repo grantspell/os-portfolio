700588a2e6c336a6d31f9a89884109f03bae4a9b
grantspell <spellgrant@gmail.com>
2018-04-02 16:01:13 -0400

    ADDED | email link in `contact`

--- client/src/components/windows/Contact.js
+++ client/src/components/windows/Contact.js
@@ -3,18 +3,31 @@ import styled from 'styled-components';
 
 // STYLES
 const ContactWrapper = styled.div`
-    height: 100%;
-    width: 100%;
     display: flex;
+`
+const LinkedIn = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+a {
+    display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    
+}
 `
 
 const Contact = () => {
     return (
         <ContactWrapper>
-            
+            <LinkedIn className="projectContainer">
+                <a href="mailto:spellGrant@gmail.com" rel="noopener noreferrer" >
+                    <img className="projectIcon" src="https://i.imgur.com/zlij372.png" alt="spellGrant@gmail.com" />
+                    <p>E-Mail</p>
+                </a>
+            </LinkedIn>
         </ContactWrapper>
     );
 };
